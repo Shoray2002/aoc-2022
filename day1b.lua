53636a38183e74a816b9093cfe0b3793f83ce47b
Shoray Singhal <shoryasinghall@gmail.com>
2022-12-01 16:50:47 +0530

    day 1 done

--- day1b.lua
+++ day1b.lua
@@ -1,15 +1,15 @@
 local checker = require("importer")
 local lines = checker.lines_from("input.txt")
-local max=0
+local total={}
 local sum = 0
+table.insert( lines, #lines+1, "" )
 for k,v in pairs(lines) do
     if #v ~= 0 then
         sum = sum + tonumber(v)
-    else
-        if sum > max then
-            max = sum
-        end
+    elseif #v == 0 then
+        table.insert(total, sum)
         sum = 0
     end
 end
-print(max)
+table.sort(total, function(a,b) return a > b end)
+print(total[1] + total[2] + total[3])

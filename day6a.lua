a7b6b2b6b72dd008ae5244ea62894cb9409b645e
Shoray Singhal <shoryasinghall@gmail.com>
2022-12-06 22:36:16 +0530

    day6 done

---
+++ day6a.lua
@@ -0,0 +1,38 @@
+local checker = require("importer")
+local lines = checker.lines_from("input.txt")
+
+
+function checkDuplicate(str)
+    local len = string.len(str)
+    local i = 1
+    local j = 1
+    local duplicate = false
+    while i <= len do
+        j = i + 1
+        while j <= len do
+            if string.sub(str, i, i) == string.sub(str, j, j) then
+                duplicate = true
+                break
+            end
+            j = j + 1
+        end
+        if duplicate then
+            break
+        end
+        i = i + 1
+    end
+    return duplicate
+end
+
+for k,v in pairs(lines) do
+    local line = v
+    local sliderSize = 4
+    for i=1,#line-sliderSize do
+        local substr = line:sub(i,i+sliderSize-1)
+        local res= checkDuplicate(substr)
+        if res == false then
+            print(substr, i+sliderSize-1)
+            break
+        end
+    end
+end
